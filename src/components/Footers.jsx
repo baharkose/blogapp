7859c4501f3006dd0ce93d83c6a0359184bcba25
baharkose <baharkse17@gmail.com>
2024-01-20 22:39:14 +0300

    added login

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Box, Container, Typography, Link } from '@mui/material';
 
 const Footers = () => {
-  return (
-    <div>
-      footer
-    </div>
-  )
-}
+    return (
+        <Box sx={{ bgcolor: 'primary.main', color: 'white', p: 3, mt: 'auto' }}>
+            <Container maxWidth="sm">
+                <Typography variant="body1" align="center">
+                    © 2024 Bahar Kose. Tüm hakları saklıdır.
+                </Typography>
+                <Typography variant="body2" align="center">
+                    <Link href="#" color="inherit">
+                        Gizlilik Politikası
+                    </Link>
+                    {' | '}
+                    <Link href="#" color="inherit">
+                        Kullanım Şartları
+                    </Link>
+                </Typography>
+            </Container>
+        </Box>
+    );
+};
 
-export default Footers
+export default Footers;

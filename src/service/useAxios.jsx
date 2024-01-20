a7487cd1a053d194b81d2a72a257360d991e104d
baharkose <baharkse17@gmail.com>
2024-01-20 23:54:47 +0300

    added useAxios hook and public axios func

--- src/service/useAxios.jsx
+++ src/service/useAxios.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import axios from "axios";
 
 const useAxios = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const axiosPublic = axios.create({
+    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+  });
 
-export default useAxios
+  return { axiosPublic };
+};
+
+export default useAxios;
